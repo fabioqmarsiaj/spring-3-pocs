f9be4976f532d4846eef1f41a3adf1ebe30217bc
Fabio Marsiaj <fabiobackend@gmail.com>
2023-12-17 20:55:43 -0300

    PostController and JsonPlaceholderService interface - fluent API for integrating with JsonPlaceHolder

--- spring-web-java-21/src/main/java/com/fabioqmarsiaj/springwebjava21/controllers/PostController.java
+++ spring-web-java-21/src/main/java/com/fabioqmarsiaj/springwebjava21/controllers/PostController.java
@@ -1,4 +1,25 @@
 package com.fabioqmarsiaj.springwebjava21.controllers;
 
+import com.fabioqmarsiaj.springwebjava21.domain.Post;
+import com.fabioqmarsiaj.springwebjava21.integrations.JsonPlaceholderService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/post")
 public class PostController {
+
+    private final JsonPlaceholderService jsonPlaceholderService;
+
+    public PostController(JsonPlaceholderService jsonPlaceholderService) {
+        this.jsonPlaceholderService = jsonPlaceholderService;
+    }
+
+    @GetMapping("")
+    List<Post> findALl() {
+        return jsonPlaceholderService.findALl();
+    }
 }
---
+++ spring-web-java-21/src/main/java/com/fabioqmarsiaj/springwebjava21/integrations/JsonPlaceholderService.java
@@ -0,0 +1,12 @@
+package com.fabioqmarsiaj.springwebjava21.integrations;
+
+import com.fabioqmarsiaj.springwebjava21.domain.Post;
+import org.springframework.web.service.annotation.GetExchange;
+
+import java.util.List;
+
+public interface JsonPlaceholderService {
+
+    @GetExchange("/posts")
+    List<Post> findALl();
+}
