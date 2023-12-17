4dce5ec512b04567eb16d2a103d54646ce658781
Fabio Marsiaj <fabiobackend@gmail.com>
2023-12-17 20:37:06 -0300

    Spring boot 3 with new RestClient fluent api and java 21

---
+++ spring-web-java-21/src/main/java/com/fabioqmarsiaj/springwebjava21/SpringWebJava21Application.java
@@ -0,0 +1,13 @@
+package com.fabioqmarsiaj.springwebjava21;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebJava21Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebJava21Application.class, args);
+	}
+
+}
---
+++ spring-web-java-21/src/test/java/com/fabioqmarsiaj/springwebjava21/SpringWebJava21ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabioqmarsiaj.springwebjava21;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebJava21ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
