cdb385d70a65d1ca5aaa8535e734df1c837c7711
Fabio Marsiaj <fabiobackend@gmail.com>
2023-12-17 21:10:16 -0300

    Enabling virtual threads

--- spring-web-java-21/src/main/java/com/fabioqmarsiaj/springwebjava21/controllers/PostController.java
+++ spring-web-java-21/src/main/java/com/fabioqmarsiaj/springwebjava21/controllers/PostController.java
@@ -2,6 +2,8 @@ package com.fabioqmarsiaj.springwebjava21.controllers;
 
 import com.fabioqmarsiaj.springwebjava21.domain.Post;
 import com.fabioqmarsiaj.springwebjava21.integrations.JsonPlaceholderService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +15,7 @@ import java.util.List;
 public class PostController {
 
     private final JsonPlaceholderService jsonPlaceholderService;
+    private static final Logger logger = LoggerFactory.getLogger(PostController.class);
 
     public PostController(JsonPlaceholderService jsonPlaceholderService) {
         this.jsonPlaceholderService = jsonPlaceholderService;
@@ -20,6 +23,7 @@ public class PostController {
 
     @GetMapping("")
     List<Post> findALl() {
+        logger.info("Current Thread: {}", Thread.currentThread());
         return jsonPlaceholderService.findAll();
     }
 }
